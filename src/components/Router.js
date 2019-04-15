04f7269e8a0e5e613d30a2a8afcb15862828a677
Ignacio Petrone <juanignaciopetrone@gmail.com>
2019-04-15 14:35:24 +0200

    probando catch of the day

--- src/components/Router.js
+++ src/components/Router.js
@@ -7,7 +7,7 @@ import NotFound from "./NotFound";
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={StorePicker} />
+      <Route exact path={process.env.PUBLIC_URL + '/'} component={StorePicker} />
       <Route path="/store/:storeId" component={App} />
       <Route component={NotFound} />
     </Switch>
